3bec6a42b26d0fee7830963d0c8a67cd1aa5b35e
perassi <horacio2jp@gmail.com>
2022-04-05 15:15:59 -0700

    kpi yml

---
+++ util/parse-kpi-yml.js
@@ -0,0 +1,39 @@
+const YAML = require('yamljs');
+const tableName = 'kpi_libs';
+const createRecord = (record) => (
+  `INSERT INTO "${tableName}"("label", "kpi") VALUES ('${record.label}', '${JSON.stringify(record.kpi)}');`
+);
+
+
+
+if (process.argv.length <= 2) {
+  console.log('YML file is required. Command should look');
+  console.log('node script/path.js path/to/yml');
+  process.exit(1);
+}
+
+const nativeObject = YAML.load(process.argv[2]);
+
+let kpiRecords = [];
+
+Object.keys(nativeObject).forEach(key => {
+  const records = nativeObject[key];
+  records.forEach(record => {
+    let exists = kpiRecords.find(el => el.label === record.label);
+
+    if (!exists) {
+      kpiRecords.push({
+        label: record.label || '',
+        kpi: {
+          formula: record.formula,
+          variables: record.variables,
+          type: record.type || '',
+        },
+      })
+    }
+  })
+});
+
+let query = kpiRecords.reduce((query, rec) => query += createRecord(rec) + '\n', '');
+
+console.log(query);
